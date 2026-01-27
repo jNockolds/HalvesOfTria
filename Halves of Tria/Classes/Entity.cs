9465dc79a59a25b62383cbec4eb90d825ffa08ee
TreeLizarde <treelizarde@gmail.com>
2026-01-27 13:41:41 +0000

    Add Entity, Component, and System classes.

---
+++ Halves of Tria/Classes/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halves_of_Tria.Classes
+{
+    class Entity
+    {
+        List<Component> components = new List<Component>();
+
+        public void AddComponent(Component component)
+        {
+            components.Add(component);
+            component.entity = this;
+        }
+
+        public T GetComponent<T>() where T : Component
+        {
+            foreach (Component component in components)
+            {
+                if (component is T)
+                {
+                    return (T)component;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Halves of Tria/Classes/System.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Halves_of_Tria.Classes
+{
+    class System<T> where T : Component
+    {
+        protected static List<T> components = new List<T>();
+        
+        public static void Register(T component)
+        {
+            components.Add(component);
+        }
+
+        public static void Update(GameTime gameTime)
+        {
+            foreach (T component in components)
+            {
+                component.Update(gameTime);
+            }
+        }
+    }
+}
