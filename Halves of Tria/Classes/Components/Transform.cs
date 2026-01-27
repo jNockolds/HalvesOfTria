8b01491bc2551def43973e021e4713ae35c0237d
TreeLizarde <treelizarde@gmail.com>
2026-01-27 14:16:39 +0000

    Add Transform Component.

---
+++ Halves of Tria/Classes/Components/Transform.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Halves_of_Tria.Classes.Components
+{
+    class Transform : Component
+    {
+        public Vector2 Position = Vector2.Zero;
+        public Vector2 Scale = Vector2.One;
+        public float Rotation = 0f;
+        public float LayerDepth = 0f;
+    }
+}
